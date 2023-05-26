9dc9f965d3bc24a0d75f89328b06c9dc53c46b22
chopthal <tjckdejr1@gmail.com>
2023-05-26 23:08:05 +0900

    Feat : slack-webhook

--- ebay-slack.js
+++ ebay-slack.js
@@ -3,41 +3,40 @@ const cheerio = require("cheerio");
 const { WebClient } = require("@slack/web-api");
 require("dotenv").config();
 
+const slackHook = process.env.SLACK_WEBHOOK;
+const searchTerm = process.env.SEARCH_TERM;
+let maxIdx = parseInt(process.env.MAX_INDEX);
+
 async function searchEbayAndSendToSlack() {
-  const searchTerm = "biacore";
   const url = `https://www.ebay.com/sch/i.html?_from=R40&_nkw=${encodeURIComponent(
-    process.env.SEARCH_TERM
+    searchTerm
   )}&_sacat=0&_sop=10`;
 
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-
     const products = $(".s-item");
-
     let message = ""; // 메시지를 저장할 변수
 
     products.each((index, element) => {
-      if (index > process.env.MAX_INDEX) return;
-
-      const name = $(element).find(".s-item__title").text().trim();
+      if (index > maxIdx) return false; // break;
+      const name = $(element)
+        .find(".s-item__title")
+        .text()
+        .trim()
+        .replace("새 리스팅", "");
+      if (name === "Shop on eBay") {
+        console.log(index);
+        maxIdx = maxIdx + 1;
+        return;
+      } // continue;
       const price = $(element).find(".s-item__price").text().trim();
       const link = $(element).find(".s-item__link").attr("href");
-
-      // 메시지에 상품 정보 추가
       message += `Product Name: ${name}\nPrice: ${price}\nLink: ${link}\n-------------------\n`;
     });
 
-    // Slack 워크스페이스에 API 요청을 보내기 위한 WebClient 생성
-    const slackClient = new WebClient(process.env.SLACK_TOKEN);
-
-    // 메시지를 Slack 채널로 전송
-    const responseSlack = await slackClient.chat.postMessage({
-      channel: process.env.SLACK_CHANNEL,
-      text: message,
-    });
-
-    console.log("Message sent to Slack. Timestamp:", responseSlack.ts);
+    await axios.post(slackHook, { text: message });
+    console.log("Message sent to Slack.");
   } catch (error) {
     console.error("Error:", error);
   }
